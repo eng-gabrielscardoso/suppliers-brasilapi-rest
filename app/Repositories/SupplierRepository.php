1ef816f4c4d4f9e487d74304ad7c914521dc78f6
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2024-12-15 23:06:44 -0300

    create repository

---
+++ app/Repositories/SupplierRepository.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Contracts\SupplierRepositoryContract;
+use App\Models\Supplier;
+use Illuminate\Database\Eloquent\Collection;
+
+class SupplierRepository implements SupplierRepositoryContract
+{
+    public function all(): Collection
+    {
+        return Supplier::all();
+    }
+
+    public function create(array $data): Supplier
+    {
+        return Supplier::create($data);
+    }
+
+    public function update(array $data, int $supplier): Supplier
+    {
+        $supplier = $this->find($supplier);
+
+        $supplier->update($data);
+
+        return $supplier;
+    }
+
+    public function delete(int $supplier): void
+    {
+        $supplier = $this->find($supplier);
+        $supplier->delete();
+    }
+
+    public function find(int $supplier): Supplier
+    {
+        return Supplier::findOrFail($supplier);
+    }
+}
